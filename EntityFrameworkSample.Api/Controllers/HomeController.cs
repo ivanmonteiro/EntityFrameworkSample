100f10031a81a6c9a5204684165f05097a0daf3b
Ivan Monteiro Cantalice <ivanmonteiroc@gmail.com>
2023-09-19 16:58:35 -0300

    Add a home controller

---
+++ EntityFrameworkSample.Api/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EntityFrameworkSample.Api.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            _logger.LogInformation("HomeController.Index");
+            return Ok();
+        }
+    }
+}
